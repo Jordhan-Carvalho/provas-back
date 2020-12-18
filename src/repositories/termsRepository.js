c209937599ae6961ca038d25aef0a52a72ba0f87
Jordhan <jordhan.rdz@gmail.com>
2020-12-18 17:34:46 -0300

    query fixes

--- src/repositories/termsRepository.js
+++ src/repositories/termsRepository.js
@@ -2,13 +2,13 @@ const db = require("../database/index");
 
 async function findTermsWithClass() {
   const resp = await db.query(
-    "SELECT term.name, term.id, COUNT(*) FROM term JOIN classes ON term.id = classes.term_id GROUP BY term.name, term.id;"
+    "SELECT term.name, term.id, COUNT(*) FROM term JOIN classes ON term.id = classes.term_id GROUP BY term.name, term.id ORDER BY term.name;"
   );
   return resp.rows;
 }
 
 async function findCatsTermByTermId(termId) {
-  const query = `SELECT classes.id, classes.name, COUNT(*) FROM classes JOIN term ON classes.term_id = term.id WHERE classes.term_id = $1 GROUP BY classes.id`;
+  const query = `SELECT classes.id, classes.name, COUNT(*) FROM tests JOIN classes ON tests.class_id = classes.id JOIN term ON classes.term_id = term.id WHERE classes.term_id = $1 GROUP BY classes.id`;
   const res = await db.query(query, [termId]);
   return res.rows;
 }
